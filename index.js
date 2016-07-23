97ed23896f88ca9f93baa89cd16c20aa6f1b645a
Linda Chang <linda_chang@brown.edu>
2016-07-23 16:38:30 -0700

    testing

--- index.js
+++ index.js
@@ -35,8 +35,8 @@ app.post('/webhook/', function (req, res) {
         sender = event.sender.id
         if (event.message && event.message.text) {
             text = event.message.text
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-            //receivedMessage();
+            //sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+            receivedMessage(event);
         }
     }
     res.sendStatus(200)
@@ -45,105 +45,105 @@ app.post('/webhook/', function (req, res) {
 var token = "EAAWVXESZCZAYYBALrnQnUpfNhDOV0xBiaLG9g5Qd30CaoYqC2TsYTrfgzMXVgwe2ZAzn7Hu4yRV2Wn8SabwAqpuefKIE4ImSJyYc6DNDtZC7GFJmzYLPdP00oZBS1kYSxlEliLCoR4mrmdHjsG7jpdlxgX7kQozOhpZCErOHWsuQZDZD"
 
 
-function sendTextMessage(sender, text) {
-    messageData = {
-        text:text
+// function sendTextMessage(sender, text) {
+//     messageData = {
+//         text:text
+//     }
+//     request({
+//         url: 'https://graph.facebook.com/v2.6/me/messages',
+//         qs: {access_token:token},
+//         method: 'POST',
+//         json: {
+//             recipient: {id:sender},
+//             message: messageData,
+//         }
+//     }, function(error, response, body) {
+//         if (error) {
+//             console.log('Error sending messages: ', error)
+//         } else if (response.body.error) {
+//             console.log('Error: ', response.body.error)
+//         }
+//     })
+// }
+
+
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:", 
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  // You may get a text or attachment but not both
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches any special
+    // keywords and send back the corresponding example. Otherwise, just echo
+    // the text we received.
+    switch (messageText) {
+    	case 'hi':
+
+    	var messageData = {
+		    recipient: {
+		      id: recipientId
+		    },
+		    message: {
+		      text: 'Hi! Welcome to Hope Cream. Would you like to know what flavors we have available?'
+		    }
+		  };
+
+  callSendAPI(messageData);
+  	break;
+      default:
+        sendTextMessage(senderID, messageText);
     }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
 }
 
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
 
-// function receivedMessage(event) {
-//   var senderID = event.sender.id;
-//   var recipientID = event.recipient.id;
-//   var timeOfMessage = event.timestamp;
-//   var message = event.message;
+  callSendAPI(messageData);
+}
 
-//   console.log("Received message for user %d and page %d at %d with message:", 
-//     senderID, recipientID, timeOfMessage);
-//   console.log(JSON.stringify(message));
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
 
-//   var messageId = message.mid;
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
 
-//   // You may get a text or attachment but not both
-//   var messageText = message.text;
-//   var messageAttachments = message.attachments;
-
-//   if (messageText) {
-
-//     // If we receive a text message, check to see if it matches any special
-//     // keywords and send back the corresponding example. Otherwise, just echo
-//     // the text we received.
-//     switch (messageText) {
-//     	case 'hi':
-
-//     	var messageData = {
-// 		    recipient: {
-// 		      id: recipientId
-// 		    },
-// 		    message: {
-// 		      text: 'Hi! Welcome to Hope Cream. Would you like to know what flavors we have available?'
-// 		    }
-// 		  };
-
-//   callSendAPI(messageData);
-//   	break;
-//       default:
-//         sendTextMessage(senderID, messageText);
-//     }
-//   } else if (messageAttachments) {
-//     sendTextMessage(senderID, "Message with attachment received");
-//   }
-// }
-
-// function sendTextMessage(recipientId, messageText) {
-//   var messageData = {
-//     recipient: {
-//       id: recipientId
-//     },
-//     message: {
-//       text: messageText
-//     }
-//   };
-
-//   callSendAPI(messageData);
-// }
-
-// function callSendAPI(messageData) {
-//   request({
-//     uri: 'https://graph.facebook.com/v2.6/me/messages',
-//     qs: { access_token: PAGE_ACCESS_TOKEN },
-//     method: 'POST',
-//     json: messageData
-
-//   }, function (error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//       var recipientId = body.recipient_id;
-//       var messageId = body.message_id;
-
-//       console.log("Successfully sent generic message with id %s to recipient %s", 
-//         messageId, recipientId);
-//     } else {
-//       console.error("Unable to send message.");
-//       console.error(response);
-//       console.error(error);
-//     }
-//   });  
-// }
+      console.log("Successfully sent generic message with id %s to recipient %s", 
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });  
+}
 
 // Spin up the server
 app.listen(app.get('port'), function() {
